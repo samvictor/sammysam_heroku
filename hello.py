c712433d26ee88a6c9e6c2226d79edd2ca0c9506
Sam <sjinniss1@hotmail.com>
2015-09-08 01:44:36 -0400

    fixing error

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
-import os
-from flask import Flask
+import os, sys
+from flask import Flask, render_template, flash, url_for, g, session, request, \
+	redirect, send_from_directory, abort
 
 app = Flask(__name__)
+
+import logging
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route('/')
 def hello():
